030d1ea416d3e9248d6c96662f44d0cbdeacbac0
Yomguithereal <guillaumeplique@gmail.com>
2022-08-31 16:23:52 +0200

    Fireworks

--- main.ts
+++ main.ts
@@ -3,8 +3,22 @@ import Graph from "graphology";
 import { SerializedGraph } from "graphology-types";
 import { scaleLinear } from "d3-scale";
 import { nodeExtent } from "graphology-metrics/graph/extent";
+import { createRandom } from "pandemonium/random";
+import seedrandom from "seedrandom";
+import Papa from "papaparse";
 
-import GRAPH_DATA from "./celegans.json";
+import GRAPH_DATA from "./data/celegans.json";
+
+const RNG = seedrandom("loubar");
+const RANDOM = createRandom(RNG);
+
+const randomByte = () => RANDOM(0, 255);
+
+function randomColor() {
+  const rgb = [randomByte(), randomByte(), randomByte()];
+
+  return "#" + rgb.map((hex) => hex.toString(16)).join("");
+}
 
 const graph = Graph.from(GRAPH_DATA as SerializedGraph);
 
@@ -14,9 +28,19 @@ const sizeScale = scaleLinear()
 
 graph.updateEachNodeAttributes((node, attr) => {
   attr.size = sizeScale(attr.size);
+  attr.color = randomColor();
   return attr;
 });
 
 const container = document.getElementById("container");
 
-const renderer = new Sigma(graph, container);
+Papa.parse("data/log.csv", {
+  header: true,
+  download: true,
+  delimiter: ",",
+  complete: (data) => {
+    console.log(data);
+  },
+});
+
+new Sigma(graph, container);
