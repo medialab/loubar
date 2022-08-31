a4a066411a1234a6f749dfe86241cc28b0cef957
Yomguithereal <guillaumeplique@gmail.com>
2022-08-31 16:51:08 +0200

    Funnier firework

--- main.ts
+++ main.ts
@@ -6,6 +6,7 @@ import { nodeExtent } from "graphology-metrics/graph/extent";
 import { createRandom } from "pandemonium/random";
 import seedrandom from "seedrandom";
 import Papa from "papaparse";
+import DefaultMap from "mnemonist/default-map";
 
 import GRAPH_DATA from "./data/celegans.json";
 
@@ -28,18 +29,47 @@ const sizeScale = scaleLinear()
 
 graph.updateEachNodeAttributes((node, attr) => {
   attr.size = sizeScale(attr.size);
-  attr.color = randomColor();
+  attr.color = "#999";
   return attr;
 });
 
 const container = document.getElementById("container");
 
+const communityColors = new DefaultMap<string, string>(() => randomColor());
+
 Papa.parse("data/log.csv", {
   header: true,
   download: true,
   delimiter: ",",
-  complete: (data) => {
-    console.log(data);
+  complete: (result) => {
+    const steps = result.data as Array<{
+      nodes: string;
+      "move-to-group": string;
+      iteration: string;
+    }>;
+    let i = 0;
+
+    function drawOneStep() {
+      if (i === steps.length - 1) return;
+
+      const step = steps[i++];
+
+      const nodes = step.nodes.split("|");
+      const community = step["move-to-group"];
+      const iteration = step.iteration;
+
+      const color = communityColors.get(community);
+
+      nodes.forEach((node) => {
+        graph.setNodeAttribute(node, "color", color);
+      });
+
+      if (iteration === "0") return drawOneStep();
+
+      requestAnimationFrame(drawOneStep);
+    }
+
+    drawOneStep();
   },
 });
 
